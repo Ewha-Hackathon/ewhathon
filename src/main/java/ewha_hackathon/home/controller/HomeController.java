6776bf3fee5e8593d9c133cf8ffb4efaf4518b55
ri-naa <rina288@ewhain.net>
2024-03-17 02:58:36 +0900

    [feat] 홈 화면

--- src/main/java/ewha_hackathon/home/controller/HomeController.java
+++ src/main/java/ewha_hackathon/home/controller/HomeController.java
@@ -1,10 +1,10 @@
 package ewha_hackathon.home.controller;
 
 import ewha_hackathon.domain.User;
-import ewha_hackathon.home.DTO.HomeDto;
+import ewha_hackathon.home.DTO.HomeResponseDto;
 import ewha_hackathon.home.service.HomeService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,11 @@ public class HomeController {
     private final HomeService homeService;
 
     @GetMapping()
-    public HomeDto getHomeInfo(@AuthenticationPrincipal User user){
+    public HomeResponseDto getHomeInfo(HttpSession session) throws Exception {
+        User user = (User)session.getAttribute("user");
+        if (user == null)
+            throw new IllegalStateException("세션 없음");
+
         return homeService.getHomeInfo(user);
     }
 
--- src/main/java/ewha_hackathon/home/service/HomeService.java
+++ src/main/java/ewha_hackathon/home/service/HomeService.java
@@ -1,21 +1,30 @@
 package ewha_hackathon.home.service;
 
 import ewha_hackathon.domain.User;
-import ewha_hackathon.home.DTO.HomeDto;
+import ewha_hackathon.home.DTO.HomeResponseDto;
+import ewha_hackathon.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class HomeService {
 
-    public HomeDto getHomeInfo(User user) {
-        HomeDto homeDto = HomeDto.builder()
-                .user_name(user.getUsername())
-                .build();
+    @Autowired
+    private final EventRepository eventRepository;
 
-        return homeDto;
+    public HomeResponseDto getHomeInfo(User user) {
+
+        return HomeResponseDto.builder()
+                .user_name(user.getUsername())
+                .popularEvents(eventRepository.findByOrderByHeartCount())
+                .latestEvents(eventRepository.findByOrderByStartDate(LocalDate.now()))
+                .build();
     }
 }
