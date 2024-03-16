59b00bfbc43b2cb2c59217f21150c41cd41eba0c
jud1thDev <8w8u8@naver.com>
2024-03-16 23:53:40 +0900

    feat: suggestion컨트롤러 추가

---
+++ src/main/java/suggestion/SuggestionController.java
@@ -0,0 +1,25 @@
+package suggestion;
+
+import ewha_hackathon.domain.Suggestion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/suggestion")
+public class SuggestionController {
+
+    private final SuggestionService suggestionService;
+
+    @Autowired
+    public SuggestionController(SuggestionService suggestionService) {
+        this.suggestionService = suggestionService;
+    }
+
+    @GetMapping
+    public List<Suggestion> getAllSuggestions() {
+        return suggestionService.findAll();
+    }
+}
