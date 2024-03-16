1210a0ece3f7b81a2bc7e1e036508a426e538ab7
jud1thDev <8w8u8@naver.com>
2024-03-16 18:55:16 +0900

    repository 수정

--- src/main/java/ewha_hackathon/repository/EventRepository.java
+++ src/main/java/ewha_hackathon/repository/EventRepository.java
@@ -2,6 +2,10 @@ package ewha_hackathon.repository;
 
 import ewha_hackathon.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+import java.util.Optional;
+
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+    Optional<Event> findById(Long eventId);
 }
