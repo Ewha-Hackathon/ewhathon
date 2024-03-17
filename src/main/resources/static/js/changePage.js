541711aefe371362cef4fed6558caeef0e90c2e4
Bokjihee <bokjihee@naver.com>
2024-03-17 16:43:12 +0900

    [fe] feat: 홈화면 연결

--- src/main/resources/static/js/changePage.js
+++ src/main/resources/static/js/changePage.js
@@ -1,3 +1,8 @@
 function changePage(url){
     window.location.href = url;
 }
+
+function changePage(element){
+    let id = element.id;
+    window.location.href = "event/detail?event_id=" + id;
+}
